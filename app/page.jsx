cee7340d681b2b4c6b72de3f2bdf91c23d1c8ce9
ygari__ <56343628+ariyog@users.noreply.github.com>
2023-05-25 23:42:46 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ export default function Home() {
         <div className="container d-flex align-items-center justify-content-center" style={{ height: "90vh" }}>
             <div className="text-center">
                 <h1>ini halaman home</h1>
+                <h2>exercise kelompok 4</h2>
             </div>
         </div>
     );
